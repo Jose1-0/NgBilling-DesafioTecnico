c44407da5eba853c4e69d59345a2af72f177db91
Jose1-0 <josealedrr@gmail.com>
2025-05-25 13:34:38 -0300

    criacao das exceptions de Transacao e Conta

---
+++ DesafioTecnico/src/main/java/com/desafio/ngbilling/exception/ContaExeption.java
@@ -0,0 +1,13 @@
+package com.desafio.ngbilling.exception;
+
+public class ContaExeption extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3122230432358085796L;
+
+	public ContaExeption(String mensagem) {
+		super(mensagem);
+	}
+}
---
+++ DesafioTecnico/src/main/java/com/desafio/ngbilling/exception/TransacaoException.java
@@ -0,0 +1,13 @@
+package com.desafio.ngbilling.exception;
+
+public class TransacaoException extends RuntimeException {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1172080771596363706L;
+
+	public TransacaoException(String mensagem) {
+		super(mensagem);
+	}
+}
