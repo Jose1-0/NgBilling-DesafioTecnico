8ece95a3c7e36b9ac837a64c175e78482e7051a7
Jose1-0 <josealedrr@gmail.com>
2025-05-25 15:32:08 -0300

    adicao da controller para execucao de transacao

---
+++ DesafioTecnico/src/main/java/com/desafio/ngbilling/controller/TransacaoController.java
@@ -0,0 +1,29 @@
+package com.desafio.ngbilling.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.desafio.ngbilling.dto.ContaDTO;
+import com.desafio.ngbilling.dto.TransacaoDTO;
+import com.desafio.ngbilling.model.Conta;
+import com.desafio.ngbilling.service.impl.TransacaoService;
+
+@RestController
+@RequestMapping("/transacao")
+public class TransacaoController {
+
+	@Autowired
+	private TransacaoService transacaoService;
+	
+	@PostMapping
+	public ResponseEntity<?> realizarTransacao(@RequestBody TransacaoDTO dto){
+			Conta conta = transacaoService.realizarTransacao(dto);
+			ContaDTO contaDTO = new ContaDTO(conta.getNumeroConta(), conta.getSaldo());
+			return ResponseEntity.status(201).body(contaDTO);
+		
+	}
+}
